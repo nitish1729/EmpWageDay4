033e9cafa9d14491c6fe918565ef93a0dcca4a3e
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-20 00:48:23 +0530

    DailyEMPWages

---
+++ UCtwo_EmpDailyWage.cs
@@ -0,0 +1,27 @@
+﻿// Part time Employee & Wage
+
+int WagePerHrs = 20;
+int FullTime = 8;
+int partTime = 4;
+int NoWork = 0;
+
+Random ran = new Random();
+
+int AttenCheck = ran.Next(1, 6);
+
+if (AttenCheck == FullTime)
+
+{
+    Console.WriteLine("Emp is Present");
+    Console.WriteLine("EmpWage : " + (WagePerHrs * FullTime));
+}
+else if (AttenCheck == partTime)
+{
+    Console.WriteLine("Emp is Present for Halfday");
+    Console.WriteLine("EmpWage : " + (WagePerHrs * partTime));
+}
+else
+{
+    Console.WriteLine("Emp is Absent");
+    Console.WriteLine("EmpWage : " + (WagePerHrs * NoWork));
+}
