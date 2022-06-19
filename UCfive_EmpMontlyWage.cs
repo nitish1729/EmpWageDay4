483669abb0d49f802fb900073f91c478de2c031a
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-20 01:18:56 +0530

    Cal Monthly Wage of Emp

---
+++ UCfive_EmpMontlyWage.cs
@@ -0,0 +1,33 @@
+﻿
+// Calculating Wages for a Month  Assume 20 Working Day per Month
+
+int WagePerHrs = 20;
+int WorkingDay = 20;
+int FullTime = 8;
+int PartTime = 4;
+int NoWork = 0;
+
+for (int Day = 0; Day < WorkingDay; Day++)
+{
+	Random random = new Random();
+	int AttenCheck = random.Next(0, 9);
+	switch (AttenCheck)
+	{
+		case 8:
+			Console.WriteLine("Emp is present ");
+			Console.WriteLine("Total Wage : " + (WagePerHrs * FullTime));
+			break;
+
+		case 4:
+			Console.WriteLine("Emp is HalfDayWork ");
+			Console.WriteLine("Total Wage : " + (WagePerHrs * PartTime));
+			break;
+
+		Default:
+
+			Console.WriteLine("Emp is Absent  ");
+			Console.WriteLine("Total Wage : " + (WagePerHrs * NoWork));
+			break;
+	}
+
+}
