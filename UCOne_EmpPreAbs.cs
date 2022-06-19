b21f2dfce8fdec3a6df2d78ee55c2ba16fe9b281
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-20 00:42:44 +0530

    Emp Pre Or Abs

---
+++ UCOne_EmpPreAbs.cs
@@ -0,0 +1,15 @@
+﻿//  Check Employee is Present or Absent
+//  Use ((RANDOM)) for Attendance Check
+
+const int FullTime = 1;
+
+Random ran = new Random(0);
+int AttenCheck = ran.Next(0, 2);
+if (AttenCheck == FullTime)
+{
+    Console.WriteLine("Emp is Present");
+}
+else
+{
+    Console.WriteLine("Emp is Absent");
+}
